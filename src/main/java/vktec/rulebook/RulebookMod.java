673f647546218476df8a1f7b2eef966df866f5dd
Samadi van Koten <samadi@vktec.org.uk>
2021-03-30 10:13:10 +0100

    Set minimum values

--- src/main/java/vktec/rulebook/RulebookMod.java
+++ src/main/java/vktec/rulebook/RulebookMod.java
@@ -8,10 +8,10 @@ import net.minecraft.world.GameRules;
 
 public class RulebookMod implements ModInitializer {
 	public static GameRules.Key<GameRules.IntRule> FILL_VOLUME =
-		GameRuleRegistry.register("fillVolume", GameRules.Category.MISC, GameRuleFactory.createIntRule(0x8000));
+		GameRuleRegistry.register("fillVolume", GameRules.Category.MISC, GameRuleFactory.createIntRule(0x8000, 0));
 	// TODO: sync with server.properties
 	public static GameRules.Key<GameRules.IntRule> VIEW_DISTANCE =
-		GameRuleRegistry.register("viewDistance", GameRules.Category.MISC, GameRuleFactory.createIntRule(10));
+		GameRuleRegistry.register("viewDistance", GameRules.Category.MISC, GameRuleFactory.createIntRule(10, 0));
 
 	@Override
 	public void onInitialize() {}
